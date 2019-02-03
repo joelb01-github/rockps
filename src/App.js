677ac31a9d9389a69aa0478c1f70fba0459b884b
Joel Barenco <joelbarenco@gmail.com>
2019-02-03 17:43:44 +0000

    simplified code

--- src/App.js
+++ src/App.js
@@ -4,106 +4,92 @@ import rockps from './contracts/rockps';
 import web3 from './web3/web3';
 class App extends Component {
 
-  state = {
-    players: [{
-      name: 'Alice',
-      status: '...loading',
-      choice: 0
-    }, {
-      name: 'Bob',
-      status: '...loading',
-      choice: 0
-    }],
-    playerCount: 0,
-    canBeFinalised: '...loading', // true if 2 players playing
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [
+        { addr: '...loading', status: '...loading'}, 
+        { addr: '...loading', status: '...loading'} ],
+      playerCount: 0,
+      canBeFinalised: '...loading', // true if 2 players playing
+      input0: 0, // to check if problem here
+      input1: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
 
   async componentDidMount() {
-    const playerCount = await rockps.methods.getPlayerCount().call();
-    const hasAlicePlayed = await (rockps.methods.hasPlayed(1).call()) 
-      ? 'has already played'
-      :'has not played yet';
-    const hasBobPlayed = await (rockps.methods.hasPlayed(2).call()) 
-      ? 'has already played'
-      :'has not played yet';
-    const canBeFinalised = 'no';
-    if (playerCount === 2) {
-      canBeFinalised = 'yes';
-    }
+    const playerCount = parseInt(await rockps.methods.getPlayerCount().call());
+    const playerAddr = await rockps.methods.getPlayers().call();
+    const players = [];
+    switch(playerCount) {
+      case 0:
+        players.push({ addr: '0x0', status:'has not played yet'});        players.push({ addr: '0x0', status:'has not played yet'});
+        break;
+      case 1:
+        players.push({ addr: playerAddr[0], status:'has already played'});
+        players.push({ addr: '0x0', status:'has not played yet'});
+        break;
+      case 2:
+        players.push({ addr: playerAddr[0], status:'has already played'});
+        players.push({ addr: playerAddr[1], status:'has already played'});
+        break;
+      default:
+    };
+    const canBeFinalised = (playerCount === 2) ? 'Yes' : 'No';
+    this.setState({ players, playerCount, canBeFinalised });
+  }
 
+  handleChange = (event, index) => {
+    const input = 'input' + index;
     this.setState({
-      playerCount, 
-      canBeFinalised,
-      players: [{...this.state.players[0],
-        status: hasAlicePlayed
-      }, {...this.state.players[1],
-        status: hasBobPlayed
-      }]
+      [input]: event.target.value
     });
   }
 
-  handleChangeAlice = (event) => {
-    this.setState({
-      players: [
-        {...this.state.players[0], choice: event.target.value},
-        {...this.state.players[1]}
-      ]
-    });
-  }
-
-  handleChangeBob = (event) => {
-    this.setState({
-      players: [
-        {...this.state.players[0]},
-        {...this.state.players[1], choice: event.target.value}
-      ]
-    });
-  }
-
-  onClickAlice = async (event) => {
-    event.preventDefault();
-    this.setState({ 
-      players: [
-        {...this.state.players[0], status: 'Sending transaction...'}, 
-        this.state.players[1] 
-      ]
-    });
-    const accounts = await web3.eth.getAccounts();
-    await rockps.methods.enterAndInput(
-      this.state.players[0].choice,
-      1 // Alice
-    ).send({ from: accounts[0] });
-    this.setState({ 
-      players: [
-        {...this.state.players[0], status: 'Transaction sent!'}, 
-        this.state.players[1] 
-      ],
-      playerCount: (parseInt(this.state.playerCount) + 1),
-      canBeFinalised: (this.state.canBeFinalised===2) ? 'Yes' : 'No'
-    });
+  handleValue = (index) => {
+    const input = 'input' + index;
+    return this.state[input];
   };
 
-  onClickBob = async (event) => {
+  handleSubmit = async (index, event) => {
     event.preventDefault();
-    this.setState({ 
-      players: [ 
-        this.state.players[0],
-        {...this.state.players[1], status: 'Sending transaction...'}
-      ]
-    });
+    switch(index) {
+      case 0:
+        this.setState({ 
+          players: [
+            {...this.state.players[0], status: 'Sending transaction...'}, 
+            this.state.players[1] 
+          ]
+        });
+        break;
+      case 1:
+        this.setState({ 
+          players: [  
+            this.state.players[0],
+            {...this.state.players[1], status: 'Sending transaction...'},
+          ]
+        });
+        break;
+      default:
+    }
     const accounts = await web3.eth.getAccounts();
-    await rockps.methods.enterAndInput(
-      this.state.players[1].choice,
-      2 // Bob
-    ).send({ from: accounts[0] });
-    this.setState({ 
-      players: [ 
-        this.state.players[0],
-        {...this.state.players[1], status: 'Transaction sent!'}
-      ],
-      playerCount: parseInt(this.state.playerCount) + 1,
-      canBeFinalised: (this.state.canBeFinalised===2) ? 'Yes' : 'No'
-    });
+    const choice = (index===0) ? this.state.input0 : this.state.input1;
+    await rockps.methods.enterAndInput(choice)
+      .send({ from: accounts[0] });
+
+    const players = (index===0) 
+      ? [ {...this.state.players[0], status: 'Transaction sent!'}, 
+        this.state.players[1] ]
+      : [...this.state.players[0],
+        {...this.state.players[1], status: 'Transaction sent!'}, ] ;
+    const playerCount = this.state.playerCount + 1;
+    const canBeFinalised = (playerCount===2) ? 'Yes' : 'No'
+
+    this.setState({ players, playerCount, canBeFinalised });
   };
 
   onClikFinal = async () => {
@@ -111,43 +97,51 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <h1>Rock Paper Scissor Game</h1>
+
+    const Header = () => {
+      return(
         <div>
-          <h2>Game status</h2>
-          <p>Currently <span className='js'>{this.state.playerCount}</span> players submitted their choice.</p>
-          <p>It the game ready to be finalised? <span className='js'>{this.state.canBeFinalised}</span></p>
-          <button onClick={this.onClikFinal}>Finalise round</button>
-          <h4>Instructions</h4>
-          <p>Choose for both Alice and Bob what they should play</p>
-          <p>When the 2 players have picked their choice and validated it (though the corresponding button), you can finalise the game</p>
-        </div>
-        <div>
+          <h1>Rock Paper Scissor Game</h1>
+          <div>
+            <p>Choose for both Alice and Bob (one after the other) what they should play</p>
+            <p>When the 2 players have sent their choice (button 'Send choice'), you can finalise the game (button 'Check winner')</p>
+          </div>
+          <div>
+            <h2>Game status</h2>
+            <p>Currently <span className='js'>{this.state.playerCount}</span> players have submitted their choice.</p>
+            <p>Is the game ready to be finalised? <span className='js'>{this.state.canBeFinalised}</span></p>
+            <button onClick={this.onClikFinal}>Check winner</button>
+          </div>
           <h2>Let's play...</h2>
-          <h4>Alice' side</h4>
-          <p>Alice' status: <span className='js'>{this.state.players[0].status}</span></p>
-          {/* <p>Alice's choice: {this.state.players[0].choice}</p> */}
-          <form onSubmit={this.onClickAlice}>
-            <label>Pick Alice's choice </label>
-            <input type='text' value={this.state.players[0].choice} onChange={this.handleChangeAlice}></input>
+        </div>
+      );
+    }
+
+    const Player = (props) => {
+      return(
+        <div>
+          <h4>{props.name}' side</h4>
+          <p>{props.name}' status: <span className='js'>{this.state.players[props.index].status}</span></p>
+          <form onSubmit={(event) => this.handleSubmit(props.index, event)}>
+            <label>Pick player's choice </label>
+            <input type='text' value={this.handleValue(props.index)} onChange={(event) => this.handleChange(event, props.index)}></input>
             <button>Send choice</button>
           </form>
           <h2>{this.state.messageAlice}</h2>
         </div>
-        <div>
-          <h4>Bob' side</h4>
-          <p>Bob' status: <span className='js'>{this.state.players[1].status}</span></p>
-          {/* <p>Bob's choice: {this.state.players[1].choice}</p> */}
-          <form onSubmit={this.onClickBob}>
-            <label>Pick Bob's choice </label>
-            <input type='text' value={this.state.players[1].choice} onChange={this.handleChangeBob}></input>
-            <button>Send choice</button>
-          </form>
-        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <Header />
+        <Player name='Alice' index={0}/>
+        <Player name='Bob' index={1}/>
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/contracts/rockps.js
+++ src/contracts/rockps.js
@@ -1,38 +1,19 @@
 import web3 from '../web3/web3';
 
-export const rockpsAddress = '0xa81dad8481841eb467d5446ece776565909289f4';
+export const rockpsAddress = '0x4d33bbdaafd478beae4ec7fc8d590c2a99c70c5b';
 export const rockpsInterface = [
 	{
-		"constant": true,
-		"inputs": [],
-		"name": "latestWinner",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
+		"constant": false,
 		"inputs": [
 			{
-				"name": "name",
+				"name": "choice",
 				"type": "uint8"
 			}
 		],
-		"name": "hasPlayed",
-		"outputs": [
-			{
-				"name": "",
-				"type": "bool"
-			}
-		],
+		"name": "enterAndInput",
+		"outputs": [],
 		"payable": false,
-		"stateMutability": "view",
+		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
@@ -42,6 +23,26 @@ export const rockpsInterface = [
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getPlayerCount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -60,36 +61,8 @@ export const rockpsInterface = [
 	},
 	{
 		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			},
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "outcomes",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "playerList",
+		"inputs": [],
+		"name": "latestWinner",
 		"outputs": [
 			{
 				"name": "",
@@ -99,71 +72,6 @@ export const rockpsInterface = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "choice",
-				"type": "uint8"
-			},
-			{
-				"name": "name",
-				"type": "uint8"
-			}
-		],
-		"name": "enterAndInput",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getPlayerCount",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "players",
-		"outputs": [
-			{
-				"name": "name",
-				"type": "uint8"
-			},
-			{
-				"name": "addr",
-				"type": "address"
-			},
-			{
-				"name": "choice",
-				"type": "uint8"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
 	}
 ];
 
