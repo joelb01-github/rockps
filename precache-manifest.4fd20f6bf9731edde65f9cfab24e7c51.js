f0c4c94c9ce03a68a24b8b110a6e6ea1af5e2a57
Joel Barenco <joelbarenco@gmail.com>
2019-02-04 09:35:43 +0000

    Updates

---
+++ precache-manifest.4fd20f6bf9731edde65f9cfab24e7c51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49243f9d659eb4db1b3a",
+    "url": "/rockps/static/js/runtime~main.49243f9d.js"
+  },
+  {
+    "revision": "c0ea225ccae2c0a97737",
+    "url": "/rockps/static/js/main.c0ea225c.chunk.js"
+  },
+  {
+    "revision": "88ae40a7bace875c4777",
+    "url": "/rockps/static/js/1.88ae40a7.chunk.js"
+  },
+  {
+    "revision": "c0ea225ccae2c0a97737",
+    "url": "/rockps/static/css/main.25a885f1.chunk.css"
+  },
+  {
+    "revision": "2fc5bc4aa69a06d522acb81c06753f77",
+    "url": "/rockps/index.html"
+  }
+];
